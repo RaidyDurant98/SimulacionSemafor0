4343ce81b968edec89303c93ff805a06af70d3eb
Raidy Duran <Raidy Duran@DESKTOP-6SH2Q4P>
2018-02-20 10:46:32 -0400

    Add project files.

---
+++ SimulaSemaforo/Simulacion.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace SimulaSemaforo
+{
+    partial class Simulacion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Startbutton = new System.Windows.Forms.Button();
+            this.Stopbutton = new System.Windows.Forms.Button();
+            this.ShutDownbutton = new System.Windows.Forms.Button();
+            this.Semaforotimer = new System.Windows.Forms.Timer(this.components);
+            this.Buttontimer = new System.Windows.Forms.Timer(this.components);
+            this.CarropictureBox = new System.Windows.Forms.PictureBox();
+            this.SemaforopictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.CarropictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SemaforopictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Startbutton
+            // 
+            this.Startbutton.Location = new System.Drawing.Point(47, 111);
+            this.Startbutton.Name = "Startbutton";
+            this.Startbutton.Size = new System.Drawing.Size(75, 23);
+            this.Startbutton.TabIndex = 1;
+            this.Startbutton.Text = "Start";
+            this.Startbutton.UseVisualStyleBackColor = true;
+            this.Startbutton.Click += new System.EventHandler(this.Startbutton_Click);
+            // 
+            // Stopbutton
+            // 
+            this.Stopbutton.Location = new System.Drawing.Point(128, 111);
+            this.Stopbutton.Name = "Stopbutton";
+            this.Stopbutton.Size = new System.Drawing.Size(75, 23);
+            this.Stopbutton.TabIndex = 2;
+            this.Stopbutton.Text = "Stop";
+            this.Stopbutton.UseVisualStyleBackColor = true;
+            this.Stopbutton.Click += new System.EventHandler(this.Stopbutton_Click);
+            // 
+            // ShutDownbutton
+            // 
+            this.ShutDownbutton.Location = new System.Drawing.Point(209, 111);
+            this.ShutDownbutton.Name = "ShutDownbutton";
+            this.ShutDownbutton.Size = new System.Drawing.Size(75, 23);
+            this.ShutDownbutton.TabIndex = 3;
+            this.ShutDownbutton.Text = "Shut Down";
+            this.ShutDownbutton.UseVisualStyleBackColor = true;
+            this.ShutDownbutton.Click += new System.EventHandler(this.ShutDownbutton_Click);
+            // 
+            // Semaforotimer
+            // 
+            this.Semaforotimer.Tick += new System.EventHandler(this.Semaforotimer_Tick);
+            // 
+            // Buttontimer
+            // 
+            this.Buttontimer.Tick += new System.EventHandler(this.Buttontimer_Tick);
+            // 
+            // CarropictureBox
+            // 
+            this.CarropictureBox.Image = global::SimulaSemaforo.Properties.Resources.Carro1;
+            this.CarropictureBox.Location = new System.Drawing.Point(12, 226);
+            this.CarropictureBox.Name = "CarropictureBox";
+            this.CarropictureBox.Size = new System.Drawing.Size(100, 63);
+            this.CarropictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.CarropictureBox.TabIndex = 4;
+            this.CarropictureBox.TabStop = false;
+            // 
+            // SemaforopictureBox
+            // 
+            this.SemaforopictureBox.Image = global::SimulaSemaforo.Properties.Resources.SemaforoPrendido;
+            this.SemaforopictureBox.Location = new System.Drawing.Point(73, 27);
+            this.SemaforopictureBox.Name = "SemaforopictureBox";
+            this.SemaforopictureBox.Size = new System.Drawing.Size(189, 62);
+            this.SemaforopictureBox.TabIndex = 0;
+            this.SemaforopictureBox.TabStop = false;
+            // 
+            // Simulacion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 395);
+            this.Controls.Add(this.CarropictureBox);
+            this.Controls.Add(this.ShutDownbutton);
+            this.Controls.Add(this.Stopbutton);
+            this.Controls.Add(this.Startbutton);
+            this.Controls.Add(this.SemaforopictureBox);
+            this.Name = "Simulacion";
+            this.Text = "Simulacion semaforo";
+            this.Load += new System.EventHandler(this.Simulacion_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.CarropictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SemaforopictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox SemaforopictureBox;
+        private System.Windows.Forms.Button Startbutton;
+        private System.Windows.Forms.Button Stopbutton;
+        private System.Windows.Forms.Button ShutDownbutton;
+        private System.Windows.Forms.Timer Semaforotimer;
+        private System.Windows.Forms.Timer Buttontimer;
+        private System.Windows.Forms.PictureBox CarropictureBox;
+    }
+}
+
---
+++ SimulaSemaforo/Simulacion.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimulaSemaforo
+{
+    public partial class Simulacion : Form
+    {
+        int estado = 0;
+
+        public Simulacion()
+        {
+            InitializeComponent();
+        }
+
+        private void Simulacion_Load(object sender, EventArgs e)
+        {
+            SemaforopictureBox.Image = Properties.Resources.SemaforoPrendido;
+            Stopbutton.Enabled = false;
+            ShutDownbutton.Enabled = false;
+        }
+
+        private void Semaforotimer_Tick(object sender, EventArgs e)
+        {
+            switch (estado)
+            {
+                case 0:
+                    SemaforopictureBox.Image = Properties.Resources.SemaforoVerde;
+                    Semaforotimer.Interval = 3500;
+                    estado = 1;
+                    Buttontimer.Start();
+                    break;
+
+                case 1:
+                    SemaforopictureBox.Image = Properties.Resources.SemaforoAmarillo;
+                    Semaforotimer.Interval = 800;
+                    estado = 2;
+                    break;
+
+                case 2:
+                    SemaforopictureBox.Image = Properties.Resources.SemaforoRojo;
+                    Semaforotimer.Interval = 3500;
+
+                    if (CarropictureBox.Location.X >= 280)
+                    {
+                        Buttontimer.Stop();
+                    }
+
+                    estado = 0;
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void Startbutton_Click(object sender, EventArgs e)
+        {
+            Semaforotimer.Start();
+            SemaforopictureBox.Image = Properties.Resources.SemaforoVerde;
+            Startbutton.Enabled = false;
+            Stopbutton.Enabled = true;
+            ShutDownbutton.Enabled = true;
+        }
+
+        private void Stopbutton_Click(object sender, EventArgs e)
+        {
+            if (Stopbutton.Text == "Stop")
+            {
+                Semaforotimer.Stop();
+                Stopbutton.Text = "Continue";
+            }
+            else
+            {
+                Semaforotimer.Start();
+                Stopbutton.Text = "Stop";
+            }
+        }
+
+        private void ShutDownbutton_Click(object sender, EventArgs e)
+        {
+            Semaforotimer.Stop();
+            SemaforopictureBox.Image = Properties.Resources.SemaforoPrendido;
+            Startbutton.Enabled = true;
+            Stopbutton.Enabled = false;
+            ShutDownbutton.Enabled = false;
+        }
+
+        private void Buttontimer_Tick(object sender, EventArgs e)
+        {
+            CarropictureBox.Location = new Point(CarropictureBox.Location.X + 5, CarropictureBox.Location.Y);
+        }
+    }
+}
